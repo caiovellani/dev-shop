52fcb38bd7899443a8639be0aca368c494f75471
Caio Vellani <caiovellani@outlook.com>
2025-01-10 18:13:39 -0300

    feat: Implement Add to Cart functionality and update product page
    - Add AddToCartButton component to product page
    - Integrate CartWidget in header
    - Update CartContext to use number for productId
    - Display installment price on product page
    - Refactor cart-related components for better integration

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import type { Product } from "../../../../data/types/product";
 import { api } from "../../../../data/api";
 import type { Metadata } from "next";
+import { AddToCartButton } from "../../../components/add-to-cart-button";
 
 type ProductProps = {
 	params: {
@@ -71,6 +72,7 @@ export default async function ProductPage({ params }: ProductProps) {
 						})}
 					</span>
 					<span className="text-sm text-zinc-400">
+						Em até 12x s/juros de{" "}
 						{(product.price / 12).toLocaleString("pt-BR", {
 							style: "currency",
 							currency: "BRL",
@@ -112,12 +114,7 @@ export default async function ProductPage({ params }: ProductProps) {
 					</div>
 				</div>
 
-				<button
-					type="button"
-					className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-				>
-					Adicionar ao carrinho
-				</button>
+				<AddToCartButton productId={product.id} />
 			</div>
 		</div>
 	);
